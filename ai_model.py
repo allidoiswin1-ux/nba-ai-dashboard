bc8bceb801f504c0b79e01e989bd68eb29c1fcd2
allidoiswin1-ux <nick.rea4@outlook.com>
2026-03-10 18:22:56 -0400

    Update ai_model.py

--- ai_model.py
+++ ai_model.py
@@ -1,1 +1,38 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
 
+
+def train_projection_model(player_data, pace_data):
+
+    df = player_data.merge(pace_data, on="TEAM_ID")
+
+    X = df[[
+        "MIN",
+        "PACE",
+        "OFF_RATING",
+        "DEF_RATING"
+    ]]
+
+    y = df["PTS"]
+
+    model = LinearRegression()
+
+    model.fit(X, y)
+
+    return model
+
+
+def generate_ai_projections(model, player_data, pace_data):
+
+    df = player_data.merge(pace_data, on="TEAM_ID")
+
+    X = df[[
+        "MIN",
+        "PACE",
+        "OFF_RATING",
+        "DEF_RATING"
+    ]]
+
+    df["AI_PROJ"] = model.predict(X)
+
+    return df
